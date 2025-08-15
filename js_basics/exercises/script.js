2b10f728257e57c806500aab5cd5a4a5f8dc3d83
klaraplivova <126472015+klaraplivova@users.noreply.github.com>
2025-08-15 22:35:39 +0200

    conditionals

--- js_basics/exercises/script.js
+++ js_basics/exercises/script.js
@@ -10,11 +10,11 @@ age = 54;
 
 // const prevent re-assign
 const pi = 3.14;
-// pi = 10; - outputs error 
+// pi = 10; - outputs error
 
 // create variable using different variable
 let a = 10;
-let b = 7 * a;  //outputs 70
+let b = 7 * a; //outputs 70
 
 const max = 57;
 const actual = max - 13;
@@ -22,7 +22,7 @@ const percentage = actual / max; // outputs 0.7719
 
 // naming conventions
 
-const MY_BIRTHDAY = "10.7.2003" // hard-coded values should be in caps
+const MY_BIRTHDAY = "10.7.2003"; // hard-coded values should be in caps
 // const myAge = someCode(BIRTHDAY) // less specific values
 
 // strings in maths
@@ -37,12 +37,11 @@ let y = 10;
 let n = 123;
 n = 12.345; // represents both integer and floating point numbers
 
-let m = 1 / 0 // outputs Infinity, greater than any number
-let o = "Hi!" / 2 // outputs NaN = computational error
+let m = 1 / 0; // outputs Infinity, greater than any number
+let o = "Hi!" / 2; // outputs NaN = computational error
 
 const myString = "123";
 const myNum = Number(myString); // converts string to number if possible
-
 
 // bigint
 
@@ -58,10 +57,11 @@ let str3 = `${str2} number ${2 + 1}`; // outputs "Hello! Hello! number 3"
 
 let str4 = `For the moment I'm going to bed because it's late
 and I wish you a good night and good luck
-with a handshake.` // bacticks can be stretched over multiple rows
-let str5 = "For the moment I'm going to bed because it's late/nand I wish you a good night and good luck/nwith a handshake." // the same can be achieved using /n
+with a handshake.`; // bacticks can be stretched over multiple rows
+let str5 =
+  "For the moment I'm going to bed because it's late/nand I wish you a good night and good luck/nwith a handshake."; // the same can be achieved using /n
 
-const bigmouth = 'I\'ve got no right to take my place…'; // this prevents using the quotation marks
+const bigmouth = "I've got no right to take my place…"; // this prevents using the quotation marks
 
 const myNum2 = 123;
 const myString2 = String(myNum2); // converts all numbers to strings
@@ -71,32 +71,32 @@ let length = text.length; // outputs 26
 let char = text.charAt(0); // outputs A
 char = text.charCodeAt(0); // outputs UTF-16 code of A
 char = text.charCodeAt(56); // outputs Nan
-let at = text.at(2); // outputs C 
+let at = text.at(2); // outputs C
 at = text.at(56); // outputs undefied
 
 let text1 = "Hello";
 let text2 = "World";
 let concat = text1.concat(" ", text2); // outputs "Hello World"
 
-let text3 = "Apple, Banana, Kiwi" 
-let part = text.slice(7) // slices out the rest of the string
-part = text3.slice(-12) // if negative it's counted from the end of the string
-part = text3.slice(7, 13) // outputs the extracted part "Banana"
-part = text3.substring(7, 6) // takes the lenght of the extracted part -> outputs "Banana"
+let text3 = "Apple, Banana, Kiwi";
+let part = text.slice(7); // slices out the rest of the string
+part = text3.slice(-12); // if negative it's counted from the end of the string
+part = text3.slice(7, 13); // outputs the extracted part "Banana"
+part = text3.substring(7, 6); // takes the lenght of the extracted part -> outputs "Banana"
 
-let toUpperCase = text3.toUpperCase() // .toLowercase does the opposite
+let toUpperCase = text3.toUpperCase(); // .toLowercase does the opposite
 
-let unicodeCharcter = "Hello world \uD800" // replaced with Unicode character
+let unicodeCharcter = "Hello world \uD800"; // replaced with Unicode character
 
 let text4 = "     Hello world     ";
 let trim = text4.trim(); // removes tabs from both sides
 let trimStart = text4.trimStart(); // removes tabs only from start, .trimEnd
 
 let text5 = "5";
-let padStart = text5.padStart(4,"0") // pads with 0 until lenht 4 -> outputs 0005, .padEnd 
+let padStart = text5.padStart(4, "0"); // pads with 0 until lenht 4 -> outputs 0005, .padEnd
 
-let text6 =  "Hello world"
-let repeat = text6.repeat(4)  // pretty self explanatory
+let text6 = "Hello world";
+let repeat = text6.repeat(4); // pretty self explanatory
 
 let text7 = "Please visit Microsoft and Microsoft!";
 let replace = text7.replace("Microsoft", "Apple"); // replaces only first match
@@ -137,23 +137,70 @@ let isGreaterBoolean = isGreaterString == isGreaterNumber; // converts to 1 == 1
 
 let isEqual = 0 == false; // regular equality checks doesn't diferentiate between 0 (or empty string) and false -> outputs true
 isEqual = null == undefined; // equal each other
-let isStrictEqual = 0 === false;  // strict equality checks the data types -> outputs false
+let isStrictEqual = 0 === false; // strict equality checks the data types -> outputs false
 isStrictEqual = null === undefined; // different types -> outputs false
 
 // Logical operators
 
-//or
-
-if (1 || 0) { // treated as true || false -> outputs "Hi"
-    console.log("Hi")
+if (1 || 0) {
+  // treated as true || false -> outputs "Hi"
+  console.log("Hi");
 }
 
 let hour = 12;
 let isWeekend = true;
 
-if (hour < 10 || hour > 18 || isWeekend) { // it is the weekend
-  console.log("The office is closed."); 
+if (hour < 10 || hour > 18 || isWeekend) {
+  // it is the weekend - only one expression has to return true
+  console.log("The office is closed.");
 }
 
-if (!(age >= 14 && age <= 19)) {}
-if (age < 14 && age > 19) {}
+let accessAllowed;
+// let age1 = prompt('How old are you?', '');
+
+if (!(age >= 14 && age <= 19)) {
+  // both expressions must return true
+  accessAllowed = false;
+} else {
+  accessAllowed = true;
+}
+
+if (age < 14 || age > 19) {
+} // one expression must return true
+
+let userName = prompt("Who's there?");
+
+if (userName === "Admin") {
+  // only one = would always be true
+  let password = prompt("Password?");
+
+  if (password === "TheMaster") {
+    alert("Welcome");
+  } else if (password === null || password === "") {
+    alert("Canceled");
+  } else {
+    alert("Wrong password");
+  }
+} else if (userName === null || userName === "") {
+  alert("Canceled");
+} else {
+  alert("Wrong user name");
+}
+
+let answer = prompt("What is the official JS name?");
+
+if (answer == "ECMAScript") {
+  alert("Correct");
+} else {
+  alert("Wrong");
+}
+
+let promptedNumber = prompt("Type in a number", 0); // the 0 creates a default string
+
+if (promptedNumber > 0) {
+    alert(1)
+} else if (value < 0) {
+    alert(-1);
+} else {
+    alert(0);
+}
