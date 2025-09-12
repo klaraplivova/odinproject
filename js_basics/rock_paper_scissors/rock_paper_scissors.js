e15bf94bf73e61f9e0d36e4c50cdeabe4013a7f7
klaraplivova <126472015+klaraplivova@users.noreply.github.com>
2025-09-12 17:21:46 +0200

    round mechanic

--- js_basics/rock_paper_scissors/rock_paper_scissors.js
+++ js_basics/rock_paper_scissors/rock_paper_scissors.js
@@ -1,22 +1,44 @@
 function getComputerChoice () {
-    let computerChoice = Math.floor(Math.random() * 100);
-    if (computerChoice < 33) {
-        return computerChoice = "rock";
-    } else if (computerChoice < 66) {
-        return computerChoice = "paper";
+    let choice = Math.random();
+    if (choice < 1/3) {
+        return "rock";
+    } else if (choice < 2/3) {
+        return "paper";
     } else {
-        return computerChoice = "scissors";
+        return "scissors";
     }
 } 
 
 function getHumanChoice () {
-    let humanChoice = prompt("Rock, paper, scissors?").toLowerCase()
-    return humanChoice
+    let choice = prompt("Rock, paper, scissors?")
+    return choice
 }
 
-let humanScore = 0;
-let computerScore = 0;
+    function playRound (humanChoice, computerChoice) {
+        humanChoice = getHumanChoice().toLowerCase();
+        computerChoice = getComputerChoice();
 
-function playRound (humanChoice, computerChoice) {
-    
-}
+        if (humanChoice === computerChoice) {
+            console.log("Draw!")
+        } else if (humanChoice === "rock" && computerChoice === "paper") {
+            console.log(`You loose: ${computerChoice} beats ${humanChoice}`);
+            computerScore: ++computerScore;
+        } else if (humanChoice === "paper" && computerChoice === "scissors") {
+            console.log(`You loose: ${computerChoice} beats ${humanChoice}`); 
+            computerScore: ++computerScore;
+        } else if (humanChoice === "scissors" && computerChoice === "rock") {
+            console.log(`You loose: ${computerChoice} beats ${humanChoice}`);
+            ++computerScore; 
+        } else if (humanChoice === "rock" && computerChoice === "scissors") {
+            console.log(`You win: ${humanChoice} beats ${computerChoice}`); 
+            ++humanScore;
+            humanScore += 1;
+        } else if (humanChoice === "paper" && computerChoice === "rock") {
+            console.log(`You win: ${humanChoiceChoice} beats ${computerChoice}`); 
+            humanScore = ++humanScore; 
+        } else {
+            console.log(`You win: ${humanChoiceChoice} beats ${computerChoice}`); 
+            humanScore = ++humanScore;
+        }    
+    }  
+
